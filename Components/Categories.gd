1b2d51f2989b0a821af08d4cf4abf305ec096eba
CarolineRoemer1998 <caroroemer@gmx.de>
2024-05-31 15:32:07 +0200

    Display dishes of category when that category is selected
    Hide dishes of other categories

--- Components/Categories.gd
+++ Components/Categories.gd
@@ -8,20 +8,30 @@ func _ready() -> void:
 	pick_category(curries)
 	
 	
+	
 func pick_category(category):
 	match category:
 		curries:
 			curries.modulate.a = 1.0
+			show_children(curries)
 			salads.modulate.a = 0.5
+			hide_children(salads)
 			desserts.modulate.a = 0.5
+			hide_children(desserts)
 		salads:
 			curries.modulate.a = 0.5
+			hide_children(curries)
 			salads.modulate.a = 1.0
+			show_children(salads)
 			desserts.modulate.a = 0.5
+			hide_children(desserts)
 		desserts:
 			curries.modulate.a = 0.5
+			hide_children(curries)
 			salads.modulate.a = 0.5
+			hide_children(salads)
 			desserts.modulate.a = 1.0
+			show_children(desserts)
 
 
 func _on_curries_stews_gui_input(event: InputEvent) -> void:
@@ -37,3 +47,13 @@ func _on_desserts_drinks_gui_input(event: InputEvent) -> void:
 func _on_salads_gui_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_click"):
 		pick_category(salads)
+
+
+func hide_children(category):
+	for child in category.get_children():
+		child.visible = false
+		
+
+func show_children(category):
+	for child in category.get_children():
+		child.visible = true
