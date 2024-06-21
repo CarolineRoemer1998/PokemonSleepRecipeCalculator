8a9d12e643cc130e5f900931d33b9dc5ff95dcb5
CarolineRoemer1998 <caroroemer@gmx.de>
2024-06-22 01:16:58 +0200

    Implemented Cooking Pot Animation when cooking
    Cooking takes 0.5 seconds, during that time, you can't click the cooking pot again

--- Components/CookingPot.gd
+++ Components/CookingPot.gd
@@ -1,25 +1,20 @@
 extends Node2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var cooking_pot_sprite: TextureRect = $CookingPotSprite
-@onready var cooking_pot_label: Label = $CookingPotLabel
 @onready var inventory: Inventory = $"../Inventory"
+@onready var cooking_time: Timer = $CookingTime
+
+@onready var cooking_pot: TextureRect = $CookingPot
+@onready var cooking_pot_lid: TextureRect = $CookingPot/CookingPotLid
+@onready var cooking_pot_inside: TextureRect = $CookingPot/CookingPotInside
+@onready var cooking_pot_label: Label = $CookingPotLabel
 
 var active : bool = false
 var hovering : bool = false
 
 func _ready() -> void:
-	cooking_pot_sprite.modulate.a = Globals.modulate_disabled
-
-func _on_cooking_pot_sprite_mouse_entered() -> void:
-	if active:
-		animation_player.play("magnify")
-		hovering = true
-
-func _on_cooking_pot_sprite_mouse_exited() -> void:
-	if active:
-		animation_player.play("demagnify")
-		hovering = false
+	cooking_pot.modulate.a = Globals.modulate_disabled
+	cooking_pot_inside.visible = false
 
 func show_label():
 	cooking_pot_label.visible = true
@@ -27,21 +22,51 @@ func show_label():
 func hide_label():
 	cooking_pot_label.visible = false
 
-
-func _on_cooking_pot_sprite_gui_input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("left_click") and active:
-		inventory.remove_ingredients_from_selected_recipe()
-
 func set_visibility():
 	if inventory.selected_dish != null and inventory.selected_dish.is_cookable:
 		active = true
-		cooking_pot_sprite.modulate.a = Globals.modulate_enabled
-		cooking_pot_sprite.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
+		cooking_pot.modulate.a = Globals.modulate_enabled
+		cooking_pot_inside.visible = true
+		#cooking_pot.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
 	else:
-		if hovering:
-			animation_player.play("demagnify")
+		print(cooking_time.time_left)
+		if cooking_time.time_left < 0.5:
+			cooking_pot.modulate.a = Globals.modulate_disabled
+			cooking_pot_inside.visible = false
+			if hovering:
+				animation_player.play("demagnify")
+		cooking_pot.mouse_default_cursor_shape = Control.CURSOR_ARROW
+		hide_label()
 		active = false
 		hovering = false
-		cooking_pot_sprite.modulate.a = Globals.modulate_disabled
-		cooking_pot_sprite.mouse_default_cursor_shape = Control.CURSOR_ARROW
-		
+
+
+func _on_cooking_pot_mouse_entered() -> void:
+	if active:
+		animation_player.play("magnify")
+		cooking_pot.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
+		hovering = true
+
+
+func _on_cooking_pot_mouse_exited() -> void:
+	if active and cooking_time.time_left == 0:
+		animation_player.play("demagnify")
+	hovering = false
+
+
+func _on_cooking_pot_gui_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("left_click") and active and cooking_time.time_left == 0.0:
+		cooking_pot.mouse_default_cursor_shape = Control.CURSOR_ARROW
+		animation_player.play("cook")
+		cooking_time.start()
+		inventory.remove_ingredients_from_selected_recipe()
+
+
+func _on_cooking_time_timeout() -> void:
+	if !hovering:
+		animation_player.play("demagnify")
+		if !active:
+			cooking_pot.modulate.a = Globals.modulate_disabled
+			cooking_pot_inside.visible = false
+	if hovering and active:
+		cooking_pot.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
